78f5c597414ef989a7c404468e7986165c35062d
AasthaGithub <49694058+AasthaGithub@users.noreply.github.com>
2020-04-21 15:03:49 +0530

    Add files via upload

---
+++ Event-Sched---Android-master/app/build/generated/source/buildConfig/androidTest/debug/amr/frost/com/eventsched/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package amr.frost.com.eventsched.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "amr.frost.com.eventsched.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
